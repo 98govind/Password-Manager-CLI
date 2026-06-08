d4a40b4086af76109750b3b9eead33280a457240
98govind <govindpandey5001@gmail.com>
2026-06-08 23:55:34 +0530

    feat: feature engineering complete

---
+++ src/day5_feature_engineering.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("../data/processed/cleaned_data.csv")
+
+# New Features
+df["password_length"] = df["password"].apply(len)
+df["has_number"] = df["password"].str.contains(r"\d").astype(int)
+
+X = df[["password_length", "has_number"]]
+y = df["strength"]
+
+scaler = StandardScaler()
+X["password_length"] = scaler.fit_transform(X[["password_length"]])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Feature engineering complete!")
+print("Train Shape:", X_train.shape)
+print("Test Shape:", X_test.shape)
