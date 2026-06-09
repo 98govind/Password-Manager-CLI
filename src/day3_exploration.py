7251cf970a4e800ad79b2459112db06da4f3d119
98govind <govindpandey5001@gmail.com>
2026-06-09 19:55:28 +0530

    fix: update file paths for direct execution

--- src/day3_exploration.py
+++ src/day3_exploration.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "data", "data.csv")
+DOCS_PATH = os.path.join(BASE_DIR, "docs")
 
 # Dataset load
-df = pd.read_csv("../data/data.csv", on_bad_lines="skip")
+df = pd.read_csv(DATA_PATH, on_bad_lines="skip")
 
 # Missing values remove
 df = df.dropna()
@@ -26,14 +31,14 @@ df["strength"].value_counts().plot(kind="bar")
 plt.title("Password Strength Distribution")
 plt.xlabel("Strength")
 plt.ylabel("Count")
-plt.savefig("../docs/password_strength_distribution.png")
+plt.savefig(os.path.join(DOCS_PATH, "password_strength_distribution.png"))
 plt.show()
 
 # Graph 2: Password Length Distribution
 df["password_length"].plot(kind="hist", bins=30)
 plt.title("Password Length Distribution")
 plt.xlabel("Password Length")
-plt.savefig("../docs/password_length_distribution.png")
+plt.savefig(os.path.join(DOCS_PATH, "password_length_distribution.png"))
 plt.show()
 
 # Graph 3: Password Length Boxplot
--- src/day4_cleaning.py
+++ src/day4_cleaning.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import os
 
-df = pd.read_csv("../data/data.csv", on_bad_lines="skip")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "data", "data.csv")
+
+
+df = pd.read_csv(DATA_PATH, on_bad_lines="skip")
 
 df = df.dropna()
 df = df.drop_duplicates()
@@ -11,6 +15,8 @@ df["strength"] = df["strength"].astype(int)
 
 os.makedirs("../data/processed", exist_ok=True)
 
-df.to_csv("../data/processed/cleaned_data.csv", index=False)
+PROCESSED_PATH = os.path.join(BASE_DIR, "data", "processed", "cleaned_data.csv")
+
+df.to_csv(PROCESSED_PATH, index=False)
 
 print("Data cleaning complete!")
